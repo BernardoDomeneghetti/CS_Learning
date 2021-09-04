f32b04d157acc9aa87687cc57232fb910cecda7f
Bernardo Domeneghetti <BernardoDomeneghetti@gmail.com>
2021-09-04 04:21:25 -0300

    Criaçãp da classe repositorio de clientes

---
+++ ByteBankSample/ByteBank.Models/Repository/CustomerRepository.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using ByteBankLib.Models.Entities;
+using ByteBankLib.Models.Exceptions;
+
+namespace ByteBankLib.Repository
+{
+    public static class CustomerRepository
+    {
+        public static Dictionary<int, Customer>RegisteredCustomers{ get; set; }
+        public static List<Customer> ListRegisteredCustomers()
+        {
+            return RegisteredCustomers.Select(q => q.Value).ToList<Customer>();
+        }
+        public static CurrentAccount GetAccountByNumber(int accountNumber)
+        {
+            if (RegisteredCustomers.ContainsKey(accountNumber))
+            {
+                return RegisteredCurrentAccounts[key: accountNumber];
+            }
+            else
+            {
+                throw new AccoutnNotFoundException("The number account was not found in the registered account's dictionary");
+            }
+        }
+    }
+}
