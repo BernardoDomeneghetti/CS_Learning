da11f54134175312b92a9fb5759ef237488db4d0
Bernardo Domeneghetti <BernardoDomeneghetti@gmail.com>
2021-10-27 17:22:52 -0300

    Correção do título da página Substituição das informações exemplo por informações reais na tela de resumo Tipo de request para AddProductToCart

---
+++ CasaDoCodigo/CasaDoCodigo/Models/Requests/AddProductToCartRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models.Requests
+{
+    public class AddProductToCartRequest
+    {
+        public int ProductCode { get; set; }
+    }
+}
